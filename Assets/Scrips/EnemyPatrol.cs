1a6bea3999ad50146261a56f3b4ea15bd3f6b77f
Ke1Nox <rodrigo200312@hotmail.com>
2024-09-18 17:06:42 -0300

    Update EnemyPatrol.cs

--- Assets/Scrips/EnemyPatrol.cs
+++ Assets/Scrips/EnemyPatrol.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float Speed;
+
+    private bool isFacingRight = true;
     void Start()
     {
         
@@ -13,6 +19,21 @@ public class EnemyPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, player.position, Speed * Time.deltaTime);
+
+        bool IsPlayerRight = transform.position.x < player.transform.position.x;
+
+        Flip(IsPlayerRight);
+    }
+
+    private void Flip(bool IsPlayerRight)
+    {
+        if ((isFacingRight && !IsPlayerRight) || (!isFacingRight && IsPlayerRight))
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
     }
 }
